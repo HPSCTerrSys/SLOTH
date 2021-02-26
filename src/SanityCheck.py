e93d1e7452826831ffaf1de6b89d1302ef055b66
workflowuser01 <n.wagner@fz-juelich.de>
2021-02-26 10:51:59 +0100

    adjust SanityCheck towards standalone script (ref #17)
    encapsulate core-functionality in function to enable import in
    other scripts as well as added __name__ == '__main__' to enable
    usage as standalone script also.

--- src/SanityCheck.py
+++ src/SanityCheck.py
@@ -26,93 +26,47 @@ def get_PlotMinMaxMid_Percentil(data2plot, lower=5, upper=95):
     '''
     return vmin, vmax, vmid
 
-parser = argparse.ArgumentParser(description='Tell me what this script can do!.')
-parser.add_argument('--infiles', '-i', type=str, nargs='+', required=True,
-                    help='full file-path')
-parser.add_argument('--varname', '-v', type=str,
-                    help='variable name to check')
-parser.add_argument('--realVarName', '-r', type=str, default=None,
-                    help='real variable name to show')
+def plot_SanityCheck(data, kind='sum', data_mask=None, figname='./SanityCheck', 
+    lowerP=2, upperP=98, **kwargs):
+    if not isinstance(data, np.ndarray):
+        print(f'data is of type {type(data)} but <class "numpy.ndarray"> is required!')
+        return None
+    if data.ndim != 3:
+        print(f'data is of dimension {data.ndim} but dimension 3 is required!')
+        return None
 
-args        = parser.parse_args()
-infiles     = args.infiles
-varname     = args.varname
-realVarName = args.realVarName if args.realVarName is not None else args.varname
+    # get / handle kwargs
+    fig_title       = kwargs.pop('fig_title', None)
+    minax_title     = kwargs.pop('minax_title', None)
+    maxax_title     = kwargs.pop('maxax_title', None)
+    kinax_title     = kwargs.pop('kinax_title', None)
+    hisax_title     = kwargs.pop('hisax_title', None)
 
-lowerP = 2
-upperP = 100-lowerP
 
-for infile in infiles:
-    filename = os.path.splitext(f'{infile}')[0]
-    #with nc.Dataset(f'{infile}', 'r') as nc_file:
-    nc_file = nc.Dataset(f'{infile}', 'r')
-    #print(f'nc_file: \n{nc_file}')
-    nc_time = nc_file.variables['time']
-    dates = nc.num2date(nc_time[:],units=nc_time.units,calendar=nc_time.calendar)
-    nc_var = nc_file.variables[f'{varname}']
-    #print(f'nc_var: \n{nc_var}')
-    #nc_var = nc_var[:15,...]
-    nt, ny, nx = nc_var.shape
-    #print(f'var.shape: {var.shape}')
-    #sys.exit(0)
+    data_min_T = np.nanmin(data, axis=0)
+    data_max_T = np.nanmax(data, axis=0)
+    if kind=='sum':
+        data_kin_T = np.nansum(data, axis=0)
+        data_kin_T[data_mask[0]] = np.nan
+    elif kind=='mean':
+        data_kin_T = np.nanmean(data, axis=0)
+        data_kin_T[data_mask[0]] = np.nan
 
-    print('###################################################################')
-    print('###################################################################')
-    var       = nc_var[...]
-    var_mask  = var.mask
-    if not var_mask.any():
-        var_mask = np.zeros(var.shape, dtype=bool)
-    var       = var.filled(fill_value=np.nan)
-    var_min_T = np.nanmin(var, axis=0)
-    var_max_T = np.nanmax(var, axis=0)
-    var_sum_T = np.nansum(var, axis=0)
-    var_sum_T[var_mask[0]] = np.nan
-
-    #clevs = [0, 2, 5, 10, 15, 20, 30, 40, 60, 80,
-    #         100, 140, 200, 300, 400, 500, 600, 800, 1000, 1200, 1500]
-    #cmap_data = [(1.0, 1.0, 1.0),
-    #         (0.3137255012989044, 0.8156862854957581, 0.8156862854957581),
-    #         (0.0, 1.0, 1.0),
-    #         (0.0, 0.8784313797950745, 0.501960813999176),
-    #         (0.0, 0.7529411911964417, 0.0),
-    #         (0.501960813999176, 0.8784313797950745, 0.0),
-    #         (1.0, 1.0, 0.0),
-    #         (1.0, 0.6274510025978088, 0.0),
-    #         (1.0, 0.0, 0.0),
-    #         (1.0, 0.125490203499794, 0.501960813999176),
-    #         (0.9411764740943909, 0.250980406999588, 1.0),
-    #         (0.501960813999176, 0.125490203499794, 1.0),
-    #         (0.250980406999588, 0.250980406999588, 1.0),
-    #         (0.125490203499794, 0.125490203499794, 0.501960813999176),
-    #         (0.125490203499794, 0.125490203499794, 0.125490203499794),
-    #         (0.501960813999176, 0.501960813999176, 0.501960813999176),
-    #         (0.8784313797950745, 0.8784313797950745, 0.8784313797950745),
-    #         (0.9333333373069763, 0.8313725590705872, 0.7372549176216125),
-    #         (0.8549019694328308, 0.6509804129600525, 0.47058823704719543),
-    #         (0.6274510025978088, 0.42352941632270813, 0.23529411852359772),
-    #         (0.4000000059604645, 0.20000000298023224, 0.0)]
-    #cmap = mcolors.ListedColormap(cmap_data, 'precipitation')
-    #norm = mcolors.BoundaryNorm(clevs, cmap.N)
     cmap = copy.copy(mpl.cm.get_cmap('Spectral'))
-    #cmap = copy.copy(mpl.cm.get_cmap('viridis'))
     cmap.set_under('black')
     cmap.set_over('magenta')
-  
+
     fig = plt.figure(figsize=(11,8), dpi=100)
-    tmp_title_str = [
-            f'Sanity-Check for {cftime.datetime.strftime(dates[0], "%Y-%m-%d")} to {cftime.datetime.strftime(dates[-1], "%Y-%m-%d")}',
-            f'Var: {realVarName} in [mm]',
-        ]
-    fig.suptitle('\n'.join(tmp_title_str))
+    fig.suptitle(fig_title)
     gs  = fig.add_gridspec(nrows=2,ncols=2)
     #gs.update(wspace=0.3, hspace=0.4)
     min_ax = fig.add_subplot(gs[0,0])
     max_ax = fig.add_subplot(gs[0,1])
-    sum_ax = fig.add_subplot(gs[1,0])
+    kin_ax = fig.add_subplot(gs[1,0])
     his_ax = fig.add_subplot(gs[1,1])
-    
-    min_ax.set_title(f'{realVarName} min')
-    tmp_vmin, tmp_vmax, tmp_vmid = get_PlotMinMaxMid_Percentil(var_min_T, lower=lowerP, upper=upperP)
+
+    min_ax.set_title(minax_title)
+    tmp_vmin, tmp_vmax, tmp_vmid = get_PlotMinMaxMid_Percentil(data_min_T, lower=lowerP, upper=upperP)
     if tmp_vmin == tmp_vmax:
         tmp_norm = None
         tmp_vmin = None
@@ -122,24 +76,24 @@ for infile in infiles:
     else:
         tmp_norm = mcolors.TwoSlopeNorm(vmin=tmp_vmin, vcenter=tmp_vmid, vmax=tmp_vmax)
     tmp_infostr = [
-            f'min: {np.nanmin(var_min_T):.2e}',
-            f'max: {np.nanmax(var_min_T):.2e}',
-            f'mean: {np.nanmean(var_min_T):.2e}',
-            f'std: {np.nanstd(var_min_T):.2e}',
-            f'q_{lowerP}: {np.nanpercentile(var_min_T, lowerP):.2e}',
-            f'q_50: {np.nanpercentile(var_min_T, 50):.2e}',
-            f'q_{upperP}: {np.nanpercentile(var_min_T, upperP):.2e}',
+            f'min: {np.nanmin(data_min_T):.2e}',
+            f'max: {np.nanmax(data_min_T):.2e}',
+            f'mean: {np.nanmean(data_min_T):.2e}',
+            f'std: {np.nanstd(data_min_T):.2e}',
+            f'q_{lowerP}: {np.nanpercentile(data_min_T, lowerP):.2e}',
+            f'q_50: {np.nanpercentile(data_min_T, 50):.2e}',
+            f'q_{upperP}: {np.nanpercentile(data_min_T, upperP):.2e}',
             ]
     min_ax.text(0.01, 0.99, '\n'.join(tmp_infostr),
                  verticalalignment='top', transform=min_ax.transAxes,
                  fontsize=8)
-    img_min = min_ax.imshow(var_min_T, origin='lower', vmin=tmp_vmin, vmax=tmp_vmax,
+    img_min = min_ax.imshow(data_min_T, origin='lower', vmin=tmp_vmin, vmax=tmp_vmax,
                             interpolation='none', cmap=cmap, norm=tmp_norm)
     fig.colorbar(img_min, ax=min_ax, extend='both')
-    
 
-    max_ax.set_title(f'{realVarName} max')
-    tmp_vmin, tmp_vmax, tmp_vmid = get_PlotMinMaxMid_Percentil(var_max_T, lower=lowerP, upper=upperP)
+
+    max_ax.set_title(maxax_title)
+    tmp_vmin, tmp_vmax, tmp_vmid = get_PlotMinMaxMid_Percentil(data_max_T, lower=lowerP, upper=upperP)
     if tmp_vmin == tmp_vmax:
         tmp_norm = None
         tmp_vmin = None
@@ -149,24 +103,24 @@ for infile in infiles:
     else:
         tmp_norm = mcolors.TwoSlopeNorm(vmin=tmp_vmin, vcenter=tmp_vmid, vmax=tmp_vmax)
     tmp_infostr = [
-            f'min: {np.nanmin(var_max_T):.2e}',
-            f'max: {np.nanmax(var_max_T):.2e}',
-            f'mean: {np.nanmean(var_max_T):.2e}',
-            f'std: {np.nanstd(var_max_T):.2e}',
-            f'q_5{lowerP}: {np.nanpercentile(var_max_T, lowerP):.2e}',
-            f'q_50: {np.nanpercentile(var_max_T, 50):.2e}',
-            f'q_{upperP}: {np.nanpercentile(var_max_T, upperP):.2e}',
+            f'min: {np.nanmin(data_max_T):.2e}',
+            f'max: {np.nanmax(data_max_T):.2e}',
+            f'mean: {np.nanmean(data_max_T):.2e}',
+            f'std: {np.nanstd(data_max_T):.2e}',
+            f'q_5{lowerP}: {np.nanpercentile(data_max_T, lowerP):.2e}',
+            f'q_50: {np.nanpercentile(data_max_T, 50):.2e}',
+            f'q_{upperP}: {np.nanpercentile(data_max_T, upperP):.2e}',
             ]
     max_ax.text(0.01, 0.99, '\n'.join(tmp_infostr),
                  verticalalignment='top', transform=max_ax.transAxes,
                  fontsize=8)
-    img_max = max_ax.imshow(var_max_T, origin='lower', vmin=tmp_vmin, vmax=tmp_vmax,
+    img_max = max_ax.imshow(data_max_T, origin='lower', vmin=tmp_vmin, vmax=tmp_vmax,
                             interpolation='none', cmap=cmap, norm=tmp_norm)
     fig.colorbar(img_max, ax=max_ax, extend='both')
 
 
-    sum_ax.set_title(f'{realVarName} sum')
-    tmp_vmin, tmp_vmax, tmp_vmid = get_PlotMinMaxMid_Percentil(var_sum_T, lower=lowerP, upper=upperP)
+    kin_ax.set_title(kinax_title)
+    tmp_vmin, tmp_vmax, tmp_vmid = get_PlotMinMaxMid_Percentil(data_kin_T, lower=lowerP, upper=upperP)
     if tmp_vmin == tmp_vmax:
         tmp_norm = None
         tmp_vmin = None
@@ -176,35 +130,126 @@ for infile in infiles:
     else:
         tmp_norm = mcolors.TwoSlopeNorm(vmin=tmp_vmin, vcenter=tmp_vmid, vmax=tmp_vmax)
     tmp_infostr = [
-            f'min: {np.nanmin(var_sum_T,):.2e}',
-            f'max: {np.nanmax(var_sum_T,):.2e}',
-            f'mean: {np.nanmean(var_sum_T,):.2e}',
-            f'std: {np.nanstd(var_sum_T,):.2e}',
-            f'q_{lowerP}: {np.nanpercentile(var_sum_T, lowerP):.2e}',
-            f'q_50: {np.nanpercentile(var_sum_T, 50):.2e}',
-            f'q_{upperP}: {np.nanpercentile(var_sum_T, upperP):.2e}',
+            f'min: {np.nanmin(data_kin_T,):.2e}',
+            f'max: {np.nanmax(data_kin_T,):.2e}',
+            f'mean: {np.nanmean(data_kin_T,):.2e}',
+            f'std: {np.nanstd(data_kin_T,):.2e}',
+            f'q_{lowerP}: {np.nanpercentile(data_kin_T, lowerP):.2e}',
+            f'q_50: {np.nanpercentile(data_kin_T, 50):.2e}',
+            f'q_{upperP}: {np.nanpercentile(data_kin_T, upperP):.2e}',
             ]
-    sum_ax.text(0.01, 0.99, '\n'.join(tmp_infostr),
-                 verticalalignment='top', transform=sum_ax.transAxes,
+    kin_ax.text(0.01, 0.99, '\n'.join(tmp_infostr),
+                 verticalalignment='top', transform=kin_ax.transAxes,
                  fontsize=8)
-    img_sum = sum_ax.imshow(var_sum_T, origin='lower', vmin=tmp_vmin, vmax=tmp_vmax,
+    img_kin = kin_ax.imshow(data_kin_T, origin='lower', vmin=tmp_vmin, vmax=tmp_vmax,
                             interpolation='none', cmap=cmap, norm=tmp_norm)
-    fig.colorbar(img_sum, ax=sum_ax, extend='both')
+    fig.colorbar(img_kin, ax=kin_ax, extend='both')
 
-    his_ax.set_title(f'{realVarName} sum - distribution')
-    #his_ax.set_xlabel(f'{realVarName} [mm / {nt} days]')
+    his_ax.set_title(hisax_title)
     his_ax.set_ylabel(f'# of occurrence')
-    hist_data = var_sum_T[~np.isnan(var_sum_T)].flatten()
+    hist_data = data_kin_T[~np.isnan(data_kin_T)].flatten()
     range_min = np.nanpercentile(hist_data, lowerP)
     #range_min = np.nanmin(hist_data)
     range_max = np.nanpercentile(hist_data, upperP)
     #range_max = np.nanmax(hist_data)
     #range_max = threshold
-    bins = int((range_max-range_min) // 10)
+    bins = int(abs((range_max-range_min) // 10))    
     print(f'BINS: {bins}')
     his_ax.hist(hist_data, log=True, range=(range_min, range_max), bins=bins)
 
-    plt.savefig(f'{filename}_SanityCheck.png')
+    plt.savefig(f'{figname}')
+    plt.close('all')
 
+    pass
+
+if __name__=='__main__':
+
+    parser = argparse.ArgumentParser(description='Tell me what this script can do!.')
+    parser.add_argument('--infile', '-i', type=str, nargs='+', required=True,
+                    help='full file-path')
+    parser.add_argument('--varname', '-v', type=str, default=None,
+                    help='variable name to check')
+
+    args        = parser.parse_args()
+    infile      = args.infile
+    in_varname  = args.varname
+
+    lowerP = 2
+    upperP = 100-lowerP
+
+    filename = os.path.splitext(f'{infile}')[0]
+    varname = in_varname if in_varname is not None else filename.split('/')[-1].replace('_ts', '')
+
+    nc_file = nc.Dataset(f'{infile}', 'r')
+    nc_time = nc_file.variables['time']
+    dates = nc.num2date(nc_time[:],units=nc_time.units,calendar=nc_time.calendar)
+    nc_var = nc_file.variables[f'{varname}']
+    nt, ny, nx = nc_var.shape
+    nc_var_unit = nc_var.units
+    var  = nc_var[...]
+    var_mask  = var.mask
+    if not var_mask.any():
+        var_mask  = np.zeros(var.shape, dtype=bool)
+        var       = var.filled(fill_value=np.nan)
+
+
+    tmp_title_str = [
+        f'Sanity-Check for {cftime.datetime.strftime(dates[0], "%Y-%m-%d")} to {cftime.datetime.strftime(dates[-1], "%Y-%m-%d")}',
+        f'Var: {varname} in [{nc_var_unit}]',
+    ]
+    fig_title    = '\n'.join(tmp_title_str)
+    figname      = f'{filename}_SanityCheck.png'
+    minax_title  = f'{varname} min'
+    maxax_title  = f'{varname} max'
+    kinax_title  = f'{varname} sum'
+    hisax_title  = f'{varname} sum - distribution'
+
+    plot_SanityCheck(data=var, data_mask=var_mask, kind='mean', figname=figname,
+        fig_title=fig_title, minax_title=minax_title, 
+        maxax_title=maxax_title, kinax_title=kinax_title,
+        hisax_title=hisax_title)
+    
     # proper close netCDF file
     nc_file.close()
+
+    # for infile in infiles:
+    #     print(f'processing {infile}')
+    #     filename = os.path.splitext(f'{infile}')[0]
+    #     varname = in_varname if in_varname is not None else filename.split('/')[-1].replace('_ts', '')
+    #     try:
+    #         nc_file = nc.Dataset(f'{infile}', 'r')
+    #         nc_time = nc_file.variables['time']
+    #         dates = nc.num2date(nc_time[:],units=nc_time.units,calendar=nc_time.calendar)
+    #         nc_var = nc_file.variables[f'{varname}']
+    #         nt, ny, nx = nc_var.shape
+    #         nc_var_unit = nc_var.units
+    #         var  = nc_var[...]
+    #         var_mask  = var.mask
+    #         if not var_mask.any():
+    #             var_mask  = np.zeros(var.shape, dtype=bool)
+    #             var       = var.filled(fill_value=np.nan)
+
+
+    #         tmp_title_str = [
+    #             f'Sanity-Check for {cftime.datetime.strftime(dates[0], "%Y-%m-%d")} to {cftime.datetime.strftime(dates[-1], "%Y-%m-%d")}',
+    #             f'Var: {varname} in [{nc_var_unit}]',
+    #         ]
+    #         fig_title    = '\n'.join(tmp_title_str)
+    #         figname      = f'{filename}_SanityCheck.png'
+    #         minax_title  = f'{varname} min'
+    #         maxax_title  = f'{varname} max'
+    #         kinax_title  = f'{varname} sum'
+    #         hisax_title  = f'{varname} sum - distribution'
+
+    #         plot_SanityCheck(data=var, data_mask=var_mask, kind='mean', figname=figname,
+    #             fig_title=fig_title, minax_title=minax_title, 
+    #             maxax_title=maxax_title, kinax_title=kinax_title,
+    #             hisax_title=hisax_title)
+            
+    #         # proper close netCDF file
+    #         nc_file.close()
+    #     except:
+    #         print(f'Unexpected error: {sys.exc_info()}')
+    #         #raise
+    #         print(f'Own ERROR while reading {filename} --> CONTINUE!')
+    #         continue
