451b9687e334818df111e96b5f1c051c1d345385
Niklas Wagner <n.wagner@fz-juelich.de>
2021-07-02 17:12:15 +0200

    BugFix in sloth/extern.py
    -) using external repos with SLOTH was not possible if the parrent
    or main script was located outside the SLOTH repositroy.
    This is fixed / workaround

--- sloth/extern.py
+++ sloth/extern.py
@@ -1,10 +1,11 @@
 # importing external repos
+import os
 import sys
-Diagnostics_path='../extern/ana_parflow-diagnostics_pythonheat'
+Diagnostics_path=f'{os.path.dirname(__file__)}/../extern/ana_parflow-diagnostics_pythonheat'
 sys.path.append(Diagnostics_path)
 import Diagnostics
 import IO as diagIO
-TrafoRotPole_path='../extern/transform_rotated_pole'
+TrafoRotPole_path=f'{os.path.dirname(__file__)}/../extern/transform_rotated_pole'
 sys.path.append(TrafoRotPole_path)
 from rotate import undo_grid_rotation
 
