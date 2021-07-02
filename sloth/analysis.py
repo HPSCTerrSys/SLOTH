b8a414e509028a44524614ca755a92df6e1d5431
Niklas Wagner <n.wagner@fz-juelich.de>
2021-07-02 17:10:51 +0200

    add and update function to calculate subsurface stats in analysis.py

--- sloth/analysis.py
+++ sloth/analysis.py
@@ -36,7 +36,7 @@ def calc_wtd(press, cellDepths):
     wtd = ht.where((wtd < 0), 0, wtd)
     return wtd
 
-def calc_gwr(spw, wtd, cellCenterDepth3D):
+def calc_gwr_v1(spw, wtd, cellCenterDepth3D):
     nz, ny, nx = cellCenterDepth3D.shape
     gwr = ht.zeros((ny, nx))
     # wtd_z_index should contain the z-index of that level which does
@@ -52,4 +52,48 @@ def calc_gwr(spw, wtd, cellCenterDepth3D):
     for z in range(nz):
         gwr = ht.where(wtd_z_index==z, spw[z], gwr)
     return gwr
+
+def calc_gwr_v2(spw, wtd_z_index):
+    spqShape = spw.shape
+    nx = spwShape[-1]
+    ny = spwShape[-2]
+    nz = spwShape[-3]
+    gwr = ht.zeros((ny, nx))
+    # wtd_z_index is the index corrospond to first cell outsid the 
+    # groundwaterbody which is to to my (NWR) unserstanding that lvl
+    # contributing to ground-water-recharge
+    for z in range(nz):
+        gwr = ht.where(wtd_z_index==z, spw[z], gwr)
+    return gwr
+
+def get_3Dgroundwaterbody_mask(satur):
+    ''' calculating a 3D mask of the groundwater-body
+
+    Return:
+      gwb_mask    = ht-ndarray
+        A HeAT-array of the same shape as 'satur' input, holding 1 at pixels
+        belonging to the groundwater-body, and 0 for other pixel
+      wtd_z_index = ht-ndarray
+        A HeAT-array of the same spatial shape as 'satur' (no z dimension),
+        holding the index of the first (first from bottom to top/surface) 
+        unsaturated layer for each pixel. If there are N layers with the model
+        and the index has a value of N+1, than the entire column is saturated!
+    '''
+    nz, ny, nx = satur.shape
+    gwb        = ht.zeros((nz+1, ny, nx))
+    gwb_mask   = ht.zeros((nz, ny, nx))
+
+    # set unsaturated pixel to 0 and saturated pixel to 1
+    # add 'zero' level on top ( ht.zeros((nz+1, ny, nx)) )
+    # to guarantee finding a min with ht.argmin()
+    gwb[:-1] = ht.where(satur>=1, 1, 0)
+    # get index of first unsaturated cell in columne, countet from model bottom
+    # this index corrospond to first cell outsid the groundwaterbody
+    wtd_z_index = ht.argmin(gwb,axis=0)
+    for z in range(nz):
+        # with `z<wtd_z_index` we do get fully saturated cells only, NOT 
+        # including the first unsaturated layer (as we want to!)
+        gwb_mask[z] = ht.where(z<wtd_z_index, 1, 0)
+
+    return gwb_mask, wtd_z_index
 
