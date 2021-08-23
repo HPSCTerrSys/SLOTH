2fe740922399200d35c6b2424837129788099927
Niklas Wagner <n.wagner@fz-juelich.de>
2021-08-23 11:27:40 +0200

    UPDATE extern.py
    -) also load rotate_grid() from "transform_rotated_pole" to SLOTH

--- sloth/extern.py
+++ sloth/extern.py
@@ -8,4 +8,5 @@ import IO as diagIO
 TrafoRotPole_path=f'{os.path.dirname(__file__)}/../extern/transform_rotated_pole'
 sys.path.append(TrafoRotPole_path)
 from rotate import undo_grid_rotation
+from rotate import rotate_grid
 
